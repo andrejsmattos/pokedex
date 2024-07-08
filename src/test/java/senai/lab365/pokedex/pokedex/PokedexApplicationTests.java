1c602e7d0a69793ab29187fc754dab1d24a35e6e
André <andrejusmattos@gmail.com>
2024-07-08 10:31:34 -0300

    commit inicial

---
+++ src/test/java/senai/lab365/pokedex/pokedex/PokedexApplicationTests.java
@@ -0,0 +1,13 @@
+package senai.lab365.pokedex.pokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
